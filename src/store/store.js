b54d7ed0dd483f37f36608ed6b10021c32166ea3
CatalinaSorina <gherzan.catalina@yahoo.com>
2019-11-25 11:02:35 +0200

    creating the store with thunk and dev-tools

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer);
+
+export default store;
